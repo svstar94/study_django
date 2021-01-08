c90990b979a37ee40352861863ea06d15695152c
wolfy <svstar94@naver.com>
2021-01-08 23:41:09 +0900

    update account views

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,8 +16,9 @@ from django.contrib.auth.forms import UserCreationForm
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+    def get_success_url(self):
+        return reverse_lazy('accountapp:login')
 
 from django.views.generic.list import MultipleObjectMixin
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -40,8 +41,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accountapp:login', kwargs={'pk':self.object.pk})
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
